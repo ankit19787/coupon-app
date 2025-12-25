dd71ca93cd162db688f305883c1614176d0c61d0
ankitbansals <ankitbansal.s@royalcyber.com>
2025-12-25 13:17:25 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://backend1-ij7ii715e-vinodpatelgroupteam.vercel.app'
+const API_URL = import.meta.env.VITE_API_URL || 'https://backend1-r00v25rf1-vinodpatelgroupteam.vercel.app'
 
 const api = axios.create({
   baseURL: API_URL,
